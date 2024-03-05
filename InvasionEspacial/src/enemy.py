69c5ad4454dbd746662c29618ae58a8d072916a3
cristxbal_vc <cristobalvalencia3002@gmail.com>
2024-03-05 12:25:45 -0500

    Ajuste de dimensiones

--- InvasionEspacial/src/enemy.py
+++ InvasionEspacial/src/enemy.py
@@ -9,8 +9,13 @@ from settings import SCREEN_WIDTH
 class Enemy:
     def __init__(self, screen):
         self.screen = screen
-        self.image = pygame.image.load(os.path.join('assets/images', 'enemy.png'))
+        # Obtiene la ruta del directorio actual donde se encuentra este script
+        current_path = os.path.dirname(__file__)  # Ruta del directorio de enemy.py
+        # Construye la ruta hacia la carpeta de assets
+        image_path = os.path.join(current_path, '../assets/images', 'enemy.png')
+        self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect()
+        # Ajusta la posición inicial del enemigo
         self.rect.x = randint(0, SCREEN_WIDTH - self.rect.width)
         self.rect.y = randint(-150, -100)
         self.speed = randint(2, 6)
--- InvasionEspacial/src/player.py
+++ InvasionEspacial/src/player.py
@@ -8,7 +8,11 @@ from settings import SCREEN_HEIGHT, SCREEN_WIDTH
 class Player:
     def __init__(self, screen):
         self.screen = screen
-        self.image = pygame.image.load(os.path.join('assets/images', 'player.png'))
+        # Obtén la ruta del directorio actual donde se encuentra este script
+        current_path = os.path.dirname(__file__)  # Ruta del directorio de player.py
+        # Construye la ruta hacia la carpeta de assets
+        image_path = os.path.join(current_path, '../assets/images', 'player.png')
+        self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect()
         self.rect.midbottom = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 10)
         self.speed = 5
